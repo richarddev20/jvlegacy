8c99e88e6632f73f2a4bebaf56b251139b0b301f
Rich Harrington <rich@maintainhq.com>
2025-11-29 20:48:51 +0000

    Fix: Add fillable fields to Person model for mass assignment

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -9,4 +9,24 @@ class Person extends Model
     protected $connection = 'legacy';
     protected $table = 'people';
     public $timestamps = false;
+
+    protected $fillable = [
+        'address_id',
+        'title',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'dob',
+        'nationality',
+        'birthplace_city',
+        'birthplace_country',
+        'email',
+        'telephone_number',
+        'mobile_number',
+        'website',
+        'income_range',
+        'occupation',
+        'avatar_id',
+        'active',
+    ];
 }
