3d72565e3cea988ee7b7cc8fea19fc3ada845380
Rich Harrington <rich@maintainhq.com>
2025-12-10 23:37:07 +0000

    Fix: Add error handling and null checks in UpdateShowController to prevent undefined array key errors

--- app/Http/Controllers/UpdateShowController.php
+++ app/Http/Controllers/UpdateShowController.php
@@ -18,16 +18,32 @@ class UpdateShowController extends Controller
             'comment' => $update->comment,
             'category' => $update->category,
             'images' => $update->images->map(function ($image) {
-                return [
-                    'url' => $image->url,
-                    'thumbnail_url' => $image->thumbnail_url,
-                    'description' => $image->description,
-                    'file_name' => $image->file_name,
-                    'file_type' => $image->file_type,
-                    'is_image' => $image->is_image,
-                    'icon' => $image->icon,
-                ];
-            }),
+                try {
+                    return [
+                        'url' => $image->url ?? '',
+                        'thumbnail_url' => $image->thumbnail_url ?? '',
+                        'description' => $image->description ?? '',
+                        'file_name' => $image->file_name ?? '',
+                        'file_type' => $image->file_type ?? '',
+                        'is_image' => $image->is_image ?? false,
+                        'icon' => $image->icon ?? 'fas fa-file text-gray-400',
+                    ];
+                } catch (\Exception $e) {
+                    // If there's an error accessing image properties, return safe defaults
+                    return [
+                        'url' => '',
+                        'thumbnail_url' => '',
+                        'description' => '',
+                        'file_name' => '',
+                        'file_type' => '',
+                        'is_image' => false,
+                        'icon' => 'fas fa-file text-gray-400',
+                    ];
+                }
+            })->filter(function ($image) {
+                // Filter out images with no URL
+                return !empty($image['url']);
+            })->values(),
         ]);
     }
 }
